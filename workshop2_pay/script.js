77133a3529241cc7eb85b4ebfc277b248c925d74
Pimlapas Mochan <pim8828@gmail.com>
2022-03-06 23:52:37 +0700

    add workshop2
    about income-expense memo

---
+++ workshop2_pay/script.js
@@ -0,0 +1,78 @@
+// reference element in index.html
+const balance = document.getElementById('balance');
+const money_plus = document.getElementById('money-plus');
+const money_minus = document.getElementById('money-minus');
+const list = document.getElementById('list');
+const form = document.getElementById('form');
+const text = document.getElementById('text');
+const amount = document.getElementById('amount');
+
+
+
+let transactions=[];
+
+function init(){
+    list.innerHTML='';
+    transactions.forEach(addDataToList);
+    calculateMoney();
+}
+
+function addDataToList(transactions){
+    const symbol= transactions.amount < 0 ?'-':'+';
+    const status= transactions.amount < 0?'minus':'plus';
+    const item=document.createElement('li');
+    result = formatNumber(Math.abs(transactions.amount));
+    item.classList.add(status);
+    item.innerHTML=`${transactions.text}<span>${symbol}${result}$</span><button class="delete-btn" onclick="removeData(${transactions.id})">x</button>`;
+    list.appendChild(item)
+}
+
+function formatNumber(num) {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+}
+
+function removeData(id){
+    transactions = transactions.filter(transactions=>transactions.id !==id)
+    init();
+}
+
+function autoID(){
+    return Math.floor(Math.random()*1000000)
+}
+
+function calculateMoney(){
+    const amounts = transactions.map(transactions=> transactions.amount);
+    const total = amounts.reduce((result,item)=>(result+=item),0).toFixed(2);
+
+    const income = amounts.filter(item=>item>0).reduce((result,item)=>(result+=item),0).toFixed(2);
+
+    const expense = (amounts.filter(item=>item<0).reduce((result,item)=>(result+=item),0)*-1).toFixed(2);
+    
+
+    balance.innerText=`฿`+formatNumber(total);
+    money_plus.innerText=`฿`+formatNumber(income);
+    money_minus.innerText=`฿`+formatNumber(expense);
+}
+
+function addTransaction(e){
+    e.preventDefault();
+    if(text.value.trim() === '' || amount.value.trim() === ''){
+        alert("Please fill the completly transaction.");
+    }else{
+        const data={
+            id:autoID(),
+            text:text.value,
+            amount:+amount.value
+        }
+        transactions.push(data);
+        addDataToList(data);
+        calculateMoney();
+        text.value='';
+        amount.value='';
+    }
+}
+
+form.addEventListener('submit',addTransaction);
+
+
+init();
