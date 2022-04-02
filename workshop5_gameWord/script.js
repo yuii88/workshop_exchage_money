4574c79c64ccd1282851c0922c85bb39759887d9
Pimlapas Mochan <pim8828@gmail.com>
2022-04-02 22:18:37 +0700

    add workshop5
    speed typing game

---
+++ workshop5_gameWord/script.js
@@ -0,0 +1,98 @@
+const wordEl=document.getElementById('word');
+const textEl=document.getElementById('text');
+const scoreEl=document.getElementById('score');
+const timeEl=document.getElementById('time');
+
+const btnLevelEl=document.getElementById('level-btn');
+const settingsEl=document.getElementById('settings');
+const levelFormEl=document.getElementById('level-form');
+const levelEl=document.getElementById('level');
+const gameoverEl=document.getElementById('gameover-container');
+
+const words =["เซ้นต์จอนชอบเด็ก","ไอปูนทำไรวะ","วิววี้ขยี้หัวใจ","ยอดเป็นสวย",
+            "ออมสินช่วยด้วย","นพเทพปะ","หงไอ้ย๊ะเห้ย","โอปอพริพริ","ไม่เล่นแม่งละ"];
+// const words =["ดีจ้า","ดี"];
+let randomText;
+let score=0;
+let time=10; //easy => 15, medium =>10, hard => 5
+const saveMode = localStorage.getItem('mode') !== null ? localStorage.getItem('mode'): 'medium';
+
+let level ='medium';
+
+
+const timeInterval=setInterval(updateTime,1000);
+
+function getRandomWord(){
+    return words[Math.floor(Math.random()*words.length)]
+}
+
+function displayWordToUI(){
+    randomText=getRandomWord();
+    wordEl.innerHTML = randomText;
+    timeEl.innerHTML = time;
+}
+textEl.addEventListener('input',(e)=>{
+    const inputText=e.target.value;
+
+    if(inputText == randomText){
+        if(saveMode =='easy'){
+            time+=5;
+        } else if (saveMode == 'medium'){
+            time+=3;
+        } else {
+            time+=2;
+        }
+        displayWordToUI();
+        updateScore();
+        e.target.value='';
+    }
+});
+
+function updateScore(){
+    score+=10;
+    scoreEl.innerHTML = score;
+}
+
+function updateTime(){
+    time--;
+    timeEl.innerHTML = time;
+    if(time==0){
+        clearInterval(timeInterval);
+        gameOver();
+    }
+}
+
+function gameOver(){
+    gameoverEl.innerHTML=`
+    <h1>END GAME!</h1>
+    <p>Your score is ${score}</p>
+    <button onclick="location.reload()">Play again</button>
+    `;
+    gameoverEl.style.display='flex';
+}
+
+btnLevelEl.addEventListener('click',()=>{
+    settingsEl.classList.toggle('hide');
+});
+
+levelEl.addEventListener('change',(e)=>{
+    level=e.target.value;
+    localStorage.setItem("mode",level);
+    
+});
+
+function startGame(){
+    levelEl.value=saveMode;
+
+    if(saveMode =='easy'){
+        time=15;
+    } else if (saveMode == 'medium'){
+        time=10;
+    } else {
+        time=5;
+    }
+    displayWordToUI();
+}
+
+startGame();
+textEl.focus();
